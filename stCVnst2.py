5eb4fcd73d5181dbf74ef826ba6ac68a7f7dc512
erichsiao1106 <65498765+erichsiao1106@users.noreply.github.com>
2021-09-17 00:16:59 +0800

    Update stCVnst2.py

--- stCVnst2.py
+++ stCVnst2.py
@@ -1,6 +1,18 @@
 import cv2
 import streamlit as st
 
+
+import streamlit as st
+import cv2 as cv
+import tempfile
+
+f = st.file_uploader("Upload file")
+tfile = tempfile.NamedTemporaryFile(delete=False)
+tfile.write(f.read())
+vf = cv.VideoCapture(tfile.name)
+
+
+
 cap = cv2.VideoCapture(0)
 st.title('Streamlit + CV2')
 run = st.checkbox('請打勾來執行')
